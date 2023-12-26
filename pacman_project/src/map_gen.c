cb240750444ba2ee8098067de905138be637fc80
anastasia <anastasia.borovik.1998@mail.ru>
2023-12-27 00:10:57 +0300

    .

--- pacman_project/src/map_gen.c
+++ pacman_project/src/map_gen.c
@@ -1,21 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <conio.h>
+#include <string.h>
+#include <ncurses.h>
+// #include <conio.h>
 
 int px = 1, py = 1;
 char map[9][45];
 char action;
 int gold_total = 0, gold_count = 0;
+char result[100] = {'0'};
 
+char * generate_filename(int lvl)
+{
+    const char * filename = "./maps/lvl_";
+    const char * ext = ".txt";
+    // char * lvl_name = lvl + '0';
+    strcpy(result, filename);
+    // strcat(result, lvl_name);
+    strcat(result, ext);
+    return result;
+}
 
 void read_map(int lvl)
 {
     int i = 0, j = 0;
-    char lvl_name = lvl + '0';
-    char filename[20] = {"./maps/lvl_", lvl_name, ".txt"};
+    
+    char * filename = generate_filename(lvl);
     char c;
     FILE * lvl_map;
-    lvl_map = fopen("./maps/lvl_1.txt", "r");
+    lvl_map = fopen(filename, "r");
     if (lvl_map) {
         while ((c = getc(lvl_map)) != EOF) {
             if (c != '\n') {
@@ -43,19 +56,18 @@ void read_map(int lvl)
 
 void check_movement(char action, int * px, int * py)
 {
-    printf("%c\n", map[*py][*px-1]);
     if ((action == 'w')&&(map[*py-1][*px]!='#')) {
             map[*py][*px] = ' ';
-            *py--;
+            (*py)--;
         } else if ((action == 's')&&(map[*py+1][*px]!='#')) {
             map[*py][*px] = ' ';
-            *py++;
+            (*py)++;
         } else if ((action == 'a')&&(map[*py][*px-1]!='#')) {
             map[*py][*px] = ' ';
-            *px--;
+            (*px)--;
         } else if ((action == 'd')&&(map[*py][*px+1]!='#')) {
             map[*py][*px] = ' ';
-            *px++;
+            (*px)++;
         }
         if (map[*py][*px]=='*') {
             gold_count += 100;
@@ -69,18 +81,30 @@ int main(int lvl)
     int x, y;
     read_map(lvl);
     do {
-        system("cls"); // cleare buffer
+        // system("cls"); // cleare buffer
+        clear();
 
         // check movements
         check_movement(action, &px, &py);
-        printf("px = %d py = %d\n", px, py);
+    //     printf("px = %d py = %d\n", px, py);
 
-        // show screen
+    //     // show screen
+    //     for (int k = 0; k < 9; k++) {
+    //         for (int l = 0; l < 45; l++) {
+    //             printf("%c", map[k][l]);
+    //         }
+    //     }
+    //     printf("Level %d. Gold: %d/%d", lvl, gold_count, gold_total);
+    // } while (action = getch());
+
+    // show screen
+    initscr();
         for (int k = 0; k < 9; k++) {
             for (int l = 0; l < 45; l++) {
-                printf("%c", map[k][l]);
+                printw("%c", map[k][l]);
             }
         }
-        printf("Level %d. Gold: %d/%d", lvl, gold_count, gold_total);
+        printw("Level %d. Gold: %d/%d", lvl, gold_count, gold_total);
     } while (action = getch());
+    endwin();
 }
