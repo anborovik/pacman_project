1e3954ec4b4a11a9a4a1239319053d27e1c0dabf
anastasia <anastasia.borovik.1998@mail.ru>
2024-01-03 01:20:50 +0300

    .

--- pacman_project/src/main.c
+++ pacman_project/src/main.c
@@ -3,7 +3,6 @@
 #include <string.h>
 #include <time.h>
 #include <ncurses.h>
-// #include <conio.h>
 #include "./modules/map_gen.h"
 #include "./modules/player.h"
 #include "./modules/monster.h"
@@ -98,39 +97,3 @@ int main(int lvl)
     }
     endwin();
 }
-
-
-// // conio for windows
-// int main(int lvl)
-// {
-//     int x, y;
-//     player_coord player = {1, 1};
-//     read_map(lvl, &player, map, &gold_total);
-//     do {
-//         // cleare buffer
-//         system("cls"); 
-
-//         // check movements
-//         check_movement(action, map, &(player.py), &(player.px), &gold_count);
-
-//         // show screen
-//         if (gold_count == gold_total) {
-//             cprintf("You win! Press Ctrl+C to exit or press Space for next level.");
-//             char key;
-//             while (key = getch()) {
-//                 if (key==' ') {
-//                     system("cls"); 
-//                     lvl++;
-//                     read_map(lvl, &player, map, &gold_total);
-//                     break;
-//                 }
-//             }
-//         }
-//         for (int k = 0; k < HEIGHT; k++) {
-//             for (int l = 0; l < WIDTH; l++) {
-//                 cprintf("%c", map[k][l]);
-//             }
-//         }
-//         cprintf("Level %d. Gold: %d/%d", lvl, gold_count, gold_total);
-//     } while (action = getch());
-// }
